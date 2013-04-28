475accde9b51055356b2fefdbb27d530845e021f
Wenyang <wenyangw@gmail.com>
2013-04-28 16:38:04 +0800

    Edit-con

--- src/lnyswz/hk/action/LsckAction.java
+++ src/lnyswz/hk/action/LsckAction.java
@@ -1,18 +1,39 @@
 package lnyswz.hk.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import lnyswz.hk.bean.Lsck;
 import lnyswz.hk.bean.User;
+import lnyswz.hk.service.LsckService;
 
 import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LsckAction extends ActionSupport {
-	HttpSession session = ServletActionContext.getRequest().getSession();
+	private LsckService lsckService;
 	
-	User user = (User)session.getAttribute("user");
-	String bmbh = user.getOrg();
-	
-	
+	@Override
+	public String execute(){
+		HttpServletRequest request = ServletActionContext.getRequest();
+		HttpSession session = request.getSession();
+		
+		User user = (User)session.getAttribute("user");
+		String bmbh = user.getOrg();
+		
+		List<Lsck> lscks = new ArrayList<Lsck>();
+		
+		request.setAttribute("lsck", lscks);
+		return null;
+
+	}
+
+	public void setLsckService(LsckService lsckService) {
+		this.lsckService = lsckService;
+	}
+		
 }
